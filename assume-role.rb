009435a3faf52a106d07d6d33b58dcb137d3a826
Hidenori Sugiyama <madogiwa@gmail.com>
2020-01-11 16:39:52 +0900

    update url

--- assume-role.rb
+++ assume-role.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class AssumeRole < Formula
-  homepage 'https://github.com/coinbase/assume-role'
-  url 'https://github.com/coinbase/assume-role/archive/v0.3.0a.tar.gz'
+  homepage 'https://github.com/madogiwa/assume-role'
+  url 'https://github.com/madogiwa/archive/v0.3.0a.tar.gz'
   sha256 '395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370'
 
   depends_on "awscli"
